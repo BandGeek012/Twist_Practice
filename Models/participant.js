2f8ffe0ee04be96437f35b11d7e9d2a03667fa95
Lab User <LabUser@labs.salina.k-state.edu>
2018-11-13 11:11:41 -0600

    11/13/2018 Created TwistDataBase Repository Crashed when trying to run;

---
+++ Models/participant.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var ParticipantSchema = new Schema(
+  {
+    ParticipantID: {type: String, required: true, max: 100}
+    LastName: {type: String, required: true, max: 100},
+    FirstName: {type: String, required: true, max: 100},
+    Address: {type: String, required: true, max: 100},
+    Email: {type: String, required: true, max: 100},
+    TimeStamp: {type: Date},
+    ParticipanType: {type: String, required: true, max: 100}
+  }
+);
+
+// Virtual for participant's full name
+ParticipantSchema
+.virtual('name')
+.get(function () {
+  return this.LastName + ', ' + this.FirstName;
+});
+
+
+
+// Virtual for participant's URL
+ParticipantSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/participant/' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('Participant', ParticipantSchema);
---
+++ routes/catalog.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+
+// Require controller modules.
+var participant_controller = require('../controllers/participantController');
+
+
+
+/// participant ROUTES ///
+
+// GET request for creating participant. NOTE This must come before route for id (i.e. display participant).
+router.get('/participant/create', participant_controller.participant_create_get);
+
+// POST request for creating participant.
+router.post('/participant/create', participant_controller.participant_create_post);
+
+// GET request to delete participant.
+router.get('/participant/:id/delete', participant_controller.participant_delete_get);
+
+// POST request to delete participant.
+router.post('/participant/:id/delete', participant_controller.participant_delete_post);
+
+// GET request to update participant.
+router.get('/participant/:id/update', participant_controller.participant_update_get);
+
+// POST request to update participant.
+router.post('/participant/:id/update', participant_controller.participant_update_post);
+
+// GET request for one participant.
+router.get('/participant/:id', participant_controller.participant_detail);
+
+// GET request for list of all participants.
+router.get('/participants', participant_controller.participant_list);
+
+
+
+
+
+module.exports = router;
